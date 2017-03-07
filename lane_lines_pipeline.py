c0f3e6c770773163a0ddf92e899ddc8d39022da4
Raz Nissim <raznissim@gmail.com>
2017-03-07 10:08:10 +0200

    Added perspective. Still fine tuning the parameters.

--- lane_lines_pipeline.py
+++ lane_lines_pipeline.py
@@ -1,15 +1,29 @@
 import cv2
 
 import calibration
+import perspective
 
 if __name__=="__main__":
 
     calibration = calibration.Calibration()
 
+    perspective = perspective.Perspective()
+
     #performing calibration
     calibration.calibrate()
-
     # testing calibration
-    test_image = cv2.imread("camera_cal/calibration2.jpg")
+    test_image = cv2.imread("test_images/straight_lines1.jpg")
     undistorted = calibration.undistort(test_image)
-    cv2.imwrite("output_images/calibration2_undistort.jpg", undistorted)
+    cv2.imwrite("output_images/straight_lines1_undistort.jpg", undistorted)
+
+    # testing perspective transform
+    warped = perspective.warp(undistorted)
+    cv2.imwrite("output_images/straight_lines1_perspective.jpg", warped)
+
+    test_image = cv2.imread("test_images/test1.jpg")
+    undistorted = calibration.undistort(test_image)
+    cv2.imwrite("output_images/test1_undistort.jpg", undistorted)
+
+    # testing perspective transform
+    warped = perspective.warp(undistorted)
+    cv2.imwrite("output_images/test1_perspective.jpg", warped)
