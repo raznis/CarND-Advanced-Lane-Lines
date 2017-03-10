Almost there. need to smooth the curve radius (maybe fix calculation?). Also, fine tune the pipeline to get the perfect result.

gradients.py
@@ -7,6 +7,30 @@ import matplotlib.pyplot as plt
 # Note: calling your function with orient='x', thresh_min=5, thresh_max=100
 # should produce output like the example image shown above this quiz.
 
+def region_of_interest(img, vertices):
+    """
+    Applies an image mask.
+
+    Only keeps the region of the image defined by the polygon
+    formed from `vertices`. The rest of the image is set to black.
+    """
+    # defining a blank mask to start with
+    mask = np.zeros_like(img)
+
+    # defining a 3 channel or 1 channel color to fill the mask with depending on the input image
+    if len(img.shape) > 2:
+        channel_count = img.shape[2]  # i.e. 3 or 4 depending on your image
+        ignore_mask_color = (255,) * channel_count
+    else:
+        ignore_mask_color = 255
+
+    # filling pixels inside the polygon defined by "vertices" with the fill color
+    cv2.fillPoly(mask, vertices, ignore_mask_color)
+
+    # returning the image only where mask pixels are nonzero
+    masked_image = cv2.bitwise_and(img, mask)
+    return masked_image
+
 
 def abs_sobel_thresh(img, orient='x', sobel_kernel=3, thresh=(0, 255)):
     # Apply the following steps to img
@@ -88,7 +112,7 @@ def filter_colors_hsv(img):
     Convert image to HSV color space and suppress any colors
     outside of the defined color ranges
     """
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
     yellow_dark = np.array([15, 127, 127], dtype=np.uint8)
     yellow_light = np.array([25, 255, 255], dtype=np.uint8)
     yellow_range = cv2.inRange(img, yellow_dark, yellow_light)
@@ -98,7 +122,8 @@ def filter_colors_hsv(img):
     white_range = cv2.inRange(img, white_dark, white_light)
     yellows_or_whites = yellow_range | white_range
     img = cv2.bitwise_and(img, img, mask=yellows_or_whites)
-    return img
+
+    return cv2.cvtColor(img, cv2.COLOR_HSV2RGB)
 
 def apply_gradient(image, ksize=7):
     # Apply each of the thresholding functions
@@ -151,6 +176,7 @@ def get_edges(image, separate_channels=False):
     -------
     Image mask with 1s in activations and 0 in other pixels.
     """
+
     # Convert to HLS color space and separate required channel
     hls = cv2.cvtColor(np.copy(image), cv2.COLOR_RGB2HLS).astype(np.float)
     s_channel = hls[:, :, 2]

perspective.py
@@ -27,24 +27,24 @@ class Perspective:
             #      [280, 30]]  # top right
             # )
 
-            src = np.float32([
-                [277, 670],
-                [581, 460],
-                [701, 460],
-                [1028, 670]
-            ])
-            dst = np.float32([
-                [200, 720],
-                [200, 0],
-                [980, 0],
-                [980, 720]
-            ])
+            # src = np.float32([
+            #     [277, 670],
+            #     [581, 460],
+            #     [701, 460],
+            #     [1028, 670]
+            # ])
+            # dst = np.float32([
+            #     [200, 720],
+            #     [200, 0],
+            #     [980, 0],
+            #     [980, 720]
+            # ])
 
-            # (h, w) = (img.shape[0], img.shape[1])
-            # # Define source points
-            # src = np.float32([[w // 2 - 76, h * .625], [w // 2 + 76, h * .625], [-100, h], [w + 100, h]])
-            # # Define corresponding destination points
-            # dst = np.float32([[100, 0], [w - 100, 0], [100, h], [w - 100, h]])
+            (h, w) = (img.shape[0], img.shape[1])
+            # Define source points
+            src = np.float32([[w // 2 - 76, h * .625], [w // 2 + 76, h * .625], [-100, h], [w + 100, h]])
+            # Define corresponding destination points
+            dst = np.float32([[100, 0], [w - 100, 0], [100, h], [w - 100, h]])
 
             self._M = cv2.getPerspectiveTransform(src, dst)
 

pipeline.py
@@ -19,23 +19,31 @@ class pipeline:
         self.perspective = perspective.Perspective()
         # performing calibration
         self.calibration.calibrate()
+        self.skipped_frames = 0
+        self.total_frames = 0
 
     def process_video(self):
         # video = 'harder_challenge_video'
         # video = 'challenge_video'
         video = 'project_video'
         white_output = '{}_done_2.mp4'.format(video)
-        clip1 = VideoFileClip('{}.mp4'.format(video)).subclip(38, 44)
+        clip1 = VideoFileClip('{}.mp4'.format(video))#.subclip(35, 44)
         white_clip = clip1.fl_image(self.process_image)  # NOTE: this function expects color images!!
         white_clip.write_videofile(white_output, audio=False)
+        return self.skipped_frames, self.total_frames
 
     def process_image(self, image):
         undistorted = self.calibration.undistort(image)
-        mask = gradients.apply_gradient(undistorted)#, separate_channels=False)
+        filtered = gradients.filter_colors_hsv(undistorted)
+        mask = gradients.get_edges(filtered, separate_channels=False)#, separate_channels=False)
         warped = np.uint8(self.perspective.warp(mask))
         # if self.left_fit is None or self.right_fit is None:
             #need to compute from scratch
-        curr_left_fit, curr_right_fit, histogram = lane_finder.first_time_lane_find(warped)
+        try:
+            curr_left_fit, curr_right_fit, histogram = lane_finder.first_time_lane_find(warped)
+        except TypeError:
+            curr_left_fit, curr_right_fit = self.left_fit, self.right_fit
+            self.skipped_frames += 1
         # else:
         #     self.left_fit, self.right_fit = lane_finder.second_time_lane_find(warped, self.left_fit, self.right_fit)
         if self.left_fit is None:
@@ -49,18 +57,21 @@ class pipeline:
 
         left_curverad, right_curverad, distance_from_center = lane_finder.compute_radius(curr_left_fit, curr_right_fit)
 
-        if curr_left_fit[0] * curr_right_fit[0] > 0.0 or (left_curverad / right_curverad < 3.0 and right_curverad / left_curverad < 3.0):
-            self.left_fit = self.left_fit * 0.8 + curr_left_fit * 0.2
-            self.right_fit = self.right_fit * 0.8 + curr_right_fit * 0.2
-
+        # if curr_left_fit[0] * curr_right_fit[0] > 0.0:
+        self.left_fit = self.left_fit * 0.8 + curr_left_fit * 0.2
+        self.right_fit = self.right_fit * 0.8 + curr_right_fit * 0.2
+        # else:
+        #     self.skipped_frames += 1
+        self.total_frames += 1
         # print(self.left_fit[0], self.right_fit[0], left_curverad, right_curverad)
 
         result = drawer.draw(undistorted, self.left_fit, self.right_fit, self.perspective, int((left_curverad + right_curverad) / 2), distance_from_center)
-        return visualization.compose_diagScreen(int((left_curverad + right_curverad) / 2), distance_from_center, result, undistorted, mask, warped)
+        return visualization.compose_diagScreen(int((left_curverad + right_curverad) / 2), distance_from_center, result, undistorted, filtered, mask, warped)
 
 
 if __name__ == "__main__":
     pipe = pipeline()
-    pipe.process_video()
+    skipped, total = pipe.process_video()
+    print("skipped frames: " + str(skipped) + " out of " + str(total))
 
 